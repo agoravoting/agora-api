2b0592ccfc2f872e76124b303096057446e55e6b
Eduardo Robles Elvira <edulix@wadobo.com>
2014-10-09 16:02:51 +0200

    fixing set_vote psql func

--- db/migrations/20140821092957_Init.sql
+++ db/migrations/20140821092957_Init.sql
@@ -18,7 +18,7 @@ CREATE UNIQUE INDEX voter_id_election_id ON votes(voter_id, election_id);
 -- RETURNS BOOLEAN AS $$ BEGIN LOOP UPDATE votes SET vote = vote, modified = current_timestamp WHERE voter_id = vid and election_id = eid; IF found THEN RETURN found; END IF; BEGIN INSERT INTO votes(vote, vote_hash, election_id, voter_id) VALUES (v, vh, eid, vid); RETURN found; EXCEPTION WHEN unique_violation THEN END; END LOOP; END; $$
 -- function on one line as goose does not seem to work otherwise
 CREATE FUNCTION set_vote(v TEXT, vh TEXT, eid TEXT, vid TEXT, theip TEXT)
-RETURNS VOID AS $$ BEGIN BEGIN INSERT INTO votes(vote, vote_hash, election_id, voter_id, ip) VALUES (v, vh, eid, vid, theip); RETURN; EXCEPTION WHEN unique_violation THEN UPDATE votes SET vote = vote, vote_hash = vh, ip = theip, modified = current_timestamp, write_count = write_count + 1 WHERE voter_id = vid and election_id = eid; IF NOT found THEN RAISE EXCEPTION 'Could not update on election_id --> %, voter_id ---> %', eid, vid; END IF; RETURN; END; END; $$
+RETURNS VOID AS $$ BEGIN BEGIN INSERT INTO votes(vote, vote_hash, election_id, voter_id, ip) VALUES (v, vh, eid, vid, theip); RETURN; EXCEPTION WHEN unique_violation THEN UPDATE votes SET vote = v, vote_hash = vh, ip = theip, modified = current_timestamp, write_count = write_count + 1 WHERE voter_id = vid and election_id = eid; IF NOT found THEN RAISE EXCEPTION 'Could not update on election_id --> %, voter_id ---> %', eid, vid; END IF; RETURN; END; END; $$
 LANGUAGE plpgsql;
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
