a85099257c22ecc4c34145a5ecdcd37c442863ca
ruescasd <fastness@gmail.com>
2014-10-19 15:50:43 +0200

    options import

--- utils/options_import.py
+++ utils/options_import.py
@@ -21,64 +21,213 @@ import json
 import copy
 import sys
 import codecs
+import re
 
-def read_csv_to_dicts(path, sep=";"):
+def read_csv_to_dicts(path):
     '''
     Given a file in CSV format, convert it to a dictionary.
     Example file (example.csv):
 
-    ID,Name,Comment
-    1,Fulanito de tal,Nothing of interest
-    2,John Doe,
+    Título de votación:,Votación de borradores
+    Descripción,Descripción de la votación lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+    URL,TÍTULO,FIRMANTES,DOCUMENTO
+    https://whatever.com,Propuesta de Pablo,"Firmante 1, Firmante 2..",Documento ético
+    https://whatever.com,Propuesta de Pablo,"Firmante 1, Firmante 2..",Documento político
+    https://whatever.com,Propuesta de Pablo,"Firmante 1, Firmante 2..",Documento organizativo
 
-    This file would be converted into the following if you call to
-    read_csv_to_dicts("example.csv", sep=","):
+    This file would be converted into the following
+
     {
-            "id": 2,
-            "media_url":"",
-            "details_title":"",
-            "details":"",
-            "value":"Resolución sobre legislación - Ley de Voto Electrónico (e-voting) ",
-            "category": "Nuevas tecnologías",
-            "a":"ballot/answer",
-            "urls":[
-              {"title": "Ver", "url": "https://web-podemos.s3.amazonaws.com/wordpress/wp-content/uploads/2014/10/REsolucionVotoElectronico.pdf"}
-            ]
+        "pretty_name": "Votación de borradores",
+        "voting_start_date": "",
+        "min": 0,
+        "max": 1,
+        "is_recurring": false,
+        "director": "wadobo-auth1",
+        "id": 1,
+        "description": "Descripción de la votación lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        "url": "http://podemos.info",
+        "questions_data": [
+            {
+                "question": "Documento organizativo",
+                "description": "",
+                "tally_type": "APPROVAL",
+                "min": 0,
+                "a": "ballot/question",
+                "max": 1,
+                "layout": "drafts-election",
+                "randomize_answer_order": false,
+                "answers": [
+                    {
+                        "media_url": "",
+                        "details_title": "",
+                        "value": "Propuesta de Pablo",
+                        "category": "",
+                        "a": "ballot/answer",
+                        "details": "Firmante 1, Firmante 2..",
+                        "urls": [
+                            {
+                                "url": "https://whatever.com",
+                                "title": "Ver"
+                            }
+                        ],
+                        "id": 3
+                    }
+                ],
+                "num_winners": 1
+            },
+            {
+                "question": "Documento político",
+                "description": "",
+                "tally_type": "APPROVAL",
+                "min": 0,
+                "a": "ballot/question",
+                "max": 1,
+                "layout": "drafts-election",
+                "randomize_answer_order": false,
+                "answers": [
+                    {
+                        "media_url": "",
+                        "details_title": "",
+                        "value": "Propuesta de Pablo",
+                        "category": "",
+                        "a": "ballot/answer",
+                        "details": "Firmante 1, Firmante 2..",
+                        "urls": [
+                            {
+                                "url": "https://whatever.com",
+                                "title": "Ver"
+                            }
+                        ],
+                        "id": 2
+                    }
+                ],
+                "num_winners": 1
+            },
+            {
+                "question": "Documento ético",
+                "description": "",
+                "tally_type": "APPROVAL",
+                "min": 0,
+                "a": "ballot/question",
+                "max": 1,
+                "layout": "drafts-election",
+                "randomize_answer_order": false,
+                "answers": [
+                    {
+                        "media_url": "",
+                        "details_title": "",
+                        "value": "Propuesta de Pablo",
+                        "category": "",
+                        "a": "ballot/answer",
+                        "details": "Firmante 1, Firmante 2..",
+                        "urls": [
+                            {
+                                "url": "https://whatever.com",
+                                "title": "Ver"
+                            }
+                        ],
+                        "id": 1
+                    }
+                ],
+                "num_winners": 1
+            }
+        ],
+        "extra": [],
+        "authorities": "openkratio-authority",
+        "layout": "drafts-election",
+        "voting_end_date": "",
+        "hash": "",
+        "title": ""
     }
+
     '''
-    ret = []
-    n = -1
-    m = 0
+
+
+    ret = {
+      "id": 1,
+      "hash": "",
+      "pretty_name":"Votación de borradores",
+      "description":"Vota los borradores para decidir la forma de organización interna de Podemos. Estas cinco resoluciones se convertirán en cinco consensos fundamentales de Podemos, cinco ejes de trabajo político colectivo, cinco hitos del proceso democratizador más grande que ha conocido este país y que estamos construyendo entre todos y todas. Porque juntos, Podemos!",
+      "title":"",
+      "layout": "drafts-election",
+      "voting_start_date": "",
+      "voting_end_date": "",
+      "is_recurring": False,
+      "director": "wadobo-auth1",
+      "authorities": "openkratio-authority",
+      "url": "http://podemos.info",
+      "extra": [],
+      "min": 0,
+      "max": 1,
+    }
+    basequestion = {
+      "a":"ballot/question",
+      "question":"", # => DOCUMENTO
+      "description": "",
+      "layout":"drafts-election",
+      "tally_type":"APPROVAL",
+      "randomize_answer_order":False,
+      "min":0,
+      "max":1,
+      "num_winners":1,
+      "answers":[]
+    }
     baseopt = {
       "id": 1,
       "media_url":"",
       "details_title":"",
-      "details":"",
-      "value":"",
+      "details":"", #=> firmantes
+      "value":"",   #=> titulo
       "category": "",
       "a":"ballot/answer",
-      "urls":[
+      "urls":[ #=> url
         {"title": "Ver", "url": ""}
       ]
     }
+    questions = {}
+    n = -1
+    m = 0
+
     with open(path, mode='r', encoding="utf-8", errors='strict') as f:
         for line in f:
             n += 1
             if n == 0:
-              continue
-            line = line.rstrip()
-            item = copy.deepcopy(baseopt)
-            values = line.split(sep)
-            if len(values) == 3:
-              m += 1
-              item['id'] = m
-              item['value'] = values[1].strip()
-              item['category'] = values[2].strip()
-              item['urls'][0]['url'] = values[0].strip()
-              ret.append(item)
+                values = line.split(',')
+                ret['pretty_name'] = values[1].strip()
+                continue
+            elif n == 1:
+                values = line.split(',')
+                ret['description'] = values[1].strip()
+                pass
+            elif n == 2:
+                # skip headers for question data
+                continue
+            else:
+                # print(n, line)
+                line = line.rstrip()
+                values = re.split(''',(?=(?:[^'"]|'[^']*'|"[^"]*")*$)''', line)
+                item = copy.deepcopy(baseopt)
+                question = values[3].strip()
+
+                if len(values) == 4:
+                    m += 1
+                    item['id'] = m
+                    item['value'] = values[1].strip()
+                    item['details'] = values[2].strip().replace('"', '')
+                    item['urls'][0]['url'] = values[0].strip()
+
+                    if question not in questions:
+                        questions[question] = copy.deepcopy(basequestion)
+                        questions[question]['question'] = question
+
+                    questions[question]['answers'].append(item)
+
+
+        ret['questions_data'] = list(questions.values())
+
     return ret
 
 if __name__ == '__main__':
-    data = read_csv_to_dicts(sys.argv[1], '\t')
-    print(json.dumps(data, indent=4, ensure_ascii=False))
-
+    data = read_csv_to_dicts(sys.argv[1])
+    print(json.dumps(data, indent=4, ensure_ascii=False, sort_keys=True))
